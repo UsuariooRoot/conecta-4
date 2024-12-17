e78bdc1732faec630bd34ba141c147b3673dcaff
burntpro <burnedprogrammer@gmail.com>
2024-12-16 20:40:18 -0500

    feat: provide context to specific routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { Route, Routes } from 'react-router'
+import { Outlet, Route, Routes } from 'react-router'
 import './App.css'
 import { WelcomeGame } from './components/welcome-game.jsx'
 import { Game } from './components/game.jsx'
-import { GameMultiplayer } from './components/game-multiplayer.jsx'
 import { GameMultiplayerPage } from './pages/game-multiplayer-page.jsx'
+import { GameMultiplayer } from './components/game-multiplayer.jsx'
+import { SocketProvider } from './context/websocket-conexion.jsx'
 
 export function App() {
   return (
     <main className='App'>
       <Routes>
         <Route path='/' element={<WelcomeGame />} />
-        <Route path='/multiplayer' element={<GameMultiplayerPage />} />
-        <Route path='/multiplayer/:id' element={<GameMultiplayer />} />
+        <Route element={<SocketProvider><Outlet /></SocketProvider>}>
+          <Route path='/multiplayer' element={<GameMultiplayerPage />} />
+          <Route path='/multiplayer/:id' element={<GameMultiplayer />} />
+        </Route>
         <Route path='/machine' element={<Game />} />
       </Routes>
     </main>
