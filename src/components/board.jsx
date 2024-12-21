d7a50901004a137c3705a4a3b7e58b6b7ab2f662
burntpro <burnedprogrammer@gmail.com>
2024-12-21 18:53:18 -0500

    fix: move the responsibility from the board to himself

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,7 +1,23 @@
+import { useRef } from 'react'
 import { N_COLUMNS } from '../const'
 import Style from './board.module.css'
 
-export function Board({ handleBoardClick, board, boardRef }) {
+export function Board({ makeMove, board }) {
+  const boardRef = useRef(null)
+
+  const handleBoardClick = (event) => {
+    const boardRect = boardRef.current.getBoundingClientRect()
+    // Checkeamos los click por cada columna
+    const clickX = event.clientX - boardRect.left
+    const columnWidth = boardRect.width / N_COLUMNS
+
+    const columnIndex = Math.floor(clickX / columnWidth)
+
+    if (columnIndex >= 0 && columnIndex < N_COLUMNS) {
+      makeMove(columnIndex)
+    }
+  }
+
   // Estilar board segun numero de columnas
   const columnsStyle = {
     gridTemplateColumns: `repeat(${N_COLUMNS}, 1fr)`
--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,29 +1,14 @@
-import { useRef } from 'react'
 import { Board } from './board'
 import { WinnerModal } from './winner-modal'
 import { useGame } from '../hooks/useGame'
-import { N_COLUMNS, TURNS } from '../const'
+import { TURNS } from '../const'
 
 export function Game() {
   const { board, turn, winner, updateBoard, resetGame } = useGame()
-  const boardRef = useRef(null)
-
-  function handleBoardClick(event) {
-    const boardRect = boardRef.current.getBoundingClientRect()
-    // Checkeamos los click por cada columna
-    const clickX = event.clientX - boardRect.left
-    const columnWidth = boardRect.width / N_COLUMNS
-
-    const columnIndex = Math.floor(clickX / columnWidth)
-
-    if (columnIndex >= 0 && columnIndex < N_COLUMNS) {
-      updateBoard(columnIndex)
-    }
-  }
 
   return (
     <>
-      <Board handleBoardClick={handleBoardClick} board={board} boardRef={boardRef} />
+      <Board makeMove={updateBoard} board={board} />
       <WinnerModal resetGame={resetGame} winner={winner} />
       <h2>{`Turno de: ${turn === TURNS.player2 ? '🔴' : '🟢'}`}</h2>
     </>
