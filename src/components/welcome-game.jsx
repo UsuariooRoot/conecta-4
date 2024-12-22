9058cd3b12191da470951bd51cd82e1508699c9c
burntpro <burnedprogrammer@gmail.com>
2024-12-22 14:06:25 -0500

    fix: change game mode selection implementation

--- src/components/welcome-game.jsx
+++ src/components/welcome-game.jsx
@@ -1,26 +1,19 @@
-import { useState } from 'react'
-import { DifficultySelectionModal, GameModeModal, InitialModal } from './welcome-modals'
+import { Link } from 'react-router'
+import { ModalContainer } from './welcome-modals'
 
 export function WelcomeGame() {
-  const [currentModal, setCurrentModal] = useState('Initial')
-
-  const openModal = (modalId) => {
-    setCurrentModal(modalId)
-  }
-
   return (
-    <div className='app'>
-      {currentModal === 'Initial' && (
-        <InitialModal nextModal={openModal} />
-      )}
-
-      {currentModal === 'GameMode' && (
-        <GameModeModal nextModal={openModal} />
-      )}
-
-      {currentModal === 'DifficultySelection' && (
-        <DifficultySelectionModal nextModal={openModal} />
-      )}
-    </div>
+    <ModalContainer>
+      <h1>Conecta 4</h1>
+      <p style={{ margin: '20px 0' }}>El juego de mesa clásico que reta tus habilidades estratégicas.</p>
+      <div className='image-placeholder'>
+        <img src='/conecta4.gif' alt='' />
+      </div>
+      <div className='game-mode-container'>
+        <h2>Elige un modo de juego</h2>
+        <Link to='/machine' className='btn-primary'>Jugar con la máquina</Link>
+        <Link to='/multiplayer' className='btn-primary'>Multijugador</Link>
+      </div>
+    </ModalContainer>
   )
 }
--- src/components/welcome-modals.jsx
+++ src/components/welcome-modals.jsx
@@ -10,29 +10,6 @@ export function ModalContainer({ children }) {
   )
 }
 
-export function InitialModal({ nextModal }) {
-  return (
-    <ModalContainer>
-      <h1>Conecta 4</h1>
-      <p style={{ margin: '20px 0' }}>El juego de mesa clásico que reta tus habilidades estratégicas.</p>
-      <div className='image-placeholder'>
-        <img src='/conecta4.gif' alt='' />
-      </div>
-      <button className='btn-primary' onClick={() => nextModal('GameMode')}>Jugar</button>
-    </ModalContainer>
-  )
-}
-
-export function GameModeModal({ nextModal }) {
-  return (
-    <ModalContainer>
-      <h2>Elige un modo de juego</h2>
-      <button className='btn-primary' onClick={() => nextModal('DifficultySelection')}>Jugar con la máquina</button>
-      <Link to='/multiplayer' className='btn-primary'>Multijugador</Link>
-    </ModalContainer>
-  )
-}
-
 export function DifficultySelectionModal() {
   return (
     <ModalContainer>
