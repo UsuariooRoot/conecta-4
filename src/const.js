2257d527eab7bc66297549e1eb5369d8cafde689
burntpro <burnedprogrammer@gmail.com>
2024-12-25 23:08:08 -0500

    clean up unused code

--- src/const.js
+++ src/const.js
@@ -1,19 +1,8 @@
-export const SELECT_TURNS = (p1, p2) => ({
-  player1: p1,
-  player2: p2
-})
-
-export const TURNS = {
-  player1: '#FF0000',
-  player2: '#00FF00'
-}
-
 export const N_ROWS = 6
-
 export const N_COLUMNS = 7
 
 export const GAME_MODE = {
-  vsMachine: 'vsMachine',
+  machine: 'machine',
   multiplayer: 'multiplayer'
 }
 
